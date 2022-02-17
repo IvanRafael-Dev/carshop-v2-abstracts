7ca169e9a4145bd2346f1028cddc3e7094416e93
Tiago Bovolin <tiagaoalb@gmail.com>
2022-02-17 14:41:12 -0300

    atualiza VehicleValidation

--- src/validations/VehicleValidation.ts
+++ src/validations/VehicleValidation.ts
@@ -1,17 +1,21 @@
 import { z } from 'zod';
 
+const TypeEnum = z.enum(['Car', 'Motorcycle', 'Truck']);
+
 const VehicleSchema = z.object({
   model: z.string().min(3, {
     message: 'model must be longer than 3 characters',
-  }).nonempty('model is required'),
+  }).nonempty({ message: 'model is required' }),
   year: z.number(),
   color: z.string().min(3, {
     message: 'color must be longer than 3 characters',
-  }).nonempty('color is required'),
+  }).nonempty({ message: 'color is required' }),
   status: z.boolean(),
   buyDate: z.date(),
   buyValue: z.number(),
-  type: z.string(),
+  type: z.string(TypeEnum).nonempty({
+    message: 'type precisa ter os valores "Car", "Motorcycle" ou "Truck"',
+  }),
 });
 
 export default VehicleSchema;
