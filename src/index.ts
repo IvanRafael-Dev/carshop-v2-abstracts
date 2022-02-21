9aa15e4d74711fdf68523f8b27e73e802f4040d4
Tiago Bovolin <tiagaoalb@gmail.com>
2022-02-21 15:37:30 -0300

    adiciona rotas para motorcycle e truck

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,22 @@
-import VehicleController from './controllers/VehicleController';
-import CarController from './controllers/CarController';
-import { Vehicle } from './interfaces/VehicleInteface';
-import { Car } from './interfaces/CarInterface';
 import CustomRouter from './routes/Router';
 import App from './server';
+
+import VehicleController from './controllers/VehicleController';
+import CarController from './controllers/CarController';
+import MotorcycleController from './controllers/MotorcycleController';
+import TruckController from './controllers/TruckController';
+
+import { Vehicle } from './interfaces/VehicleInteface';
+import { Car } from './interfaces/CarInterface';
+import { Motorcycle } from './interfaces/MotorcycleInterface';
+import { Truck } from './interfaces/TruckInterface';
 
 const server = new App();
 
 const vehicleController = new VehicleController();
 const carController = new CarController();
+const motorcycleController = new MotorcycleController();
+const truckController = new TruckController();
 
 const vehicleRouter = new CustomRouter<Vehicle>();
 vehicleRouter.addRoute(vehicleController);
@@ -16,7 +24,15 @@ vehicleRouter.addRoute(vehicleController);
 const carRouter = new CustomRouter<Car>();
 carRouter.addRoute(carController);
 
+const motorcycleRouter = new CustomRouter<Motorcycle>();
+motorcycleRouter.addRoute(motorcycleController);
+
+const truckRouter = new CustomRouter<Truck>();
+truckRouter.addRoute(truckController);
+
 server.addRouter(vehicleRouter.router);
 server.addRouter(carRouter.router);
+server.addRouter(motorcycleRouter.router);
+server.addRouter(truckRouter.router);
 
 server.startServer();
