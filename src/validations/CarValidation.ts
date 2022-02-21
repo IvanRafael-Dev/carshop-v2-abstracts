c79d747572995c03aeb5c5a1d96e15b7730ce1c1
Tiago Bovolin <tiagaoalb@gmail.com>
2022-02-21 15:35:27 -0300

    atualiza validações

--- src/validations/CarValidation.ts
+++ src/validations/CarValidation.ts
@@ -5,7 +5,11 @@ const CarSchema = VehicleSchema.extend({
   seatsQty: z.number().gte(2, {
     message: 'car must have at leat 2 seats',
   }).lte(7),
-  doorsQty: z.number(),
+  doorsQty: z.number().gte(2, {
+    message: 'car must have at leat 2 doors',
+  }).lte(4, {
+    message: 'car must not have more than 4 doors',
+  }),
 });
 
 export default CarSchema;
