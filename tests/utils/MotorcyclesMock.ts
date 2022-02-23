580df921d471a9bae74edf34b5459eacbff40c22
Tiago Bovolin <tiagaoalb@gmail.com>
2022-02-23 14:48:37 -0300

    adiciona MotorcyclesMock

---
+++ tests/utils/MotorcyclesMock.ts
@@ -0,0 +1,112 @@
+export const validMotorcyle = {
+  model: 'Honda CG Titan 125',
+  year: 1963,
+  color: 'red',
+  status: true,
+  buyValue: 3500,
+  category: 'Street',
+  engineCapacity: 125
+};
+
+export const noModelMotorcyle = {
+  year: 1963,
+  color: 'red',
+  status: true,
+  buyValue: 3500,
+  category: 'Street',
+  engineCapacity: 125
+};
+
+export const noYearMotorcyle = {
+  model: 'Honda CG Titan 125',
+  color: 'red',
+  status: true,
+  buyValue: 3500,
+  category: 'Street',
+  engineCapacity: 125
+}
+
+export const noColorMotorcyle = {
+  model: 'Honda CG Titan 125',
+  year: 1963,
+  status: true,
+  buyValue: 3500,
+  category: 'Street',
+  engineCapacity: 125
+}
+
+export const noStatusMotorcyle = {
+  model: 'Honda CG Titan 125',
+  year: 1963,
+  color: 'red',
+  buyValue: 3500,
+  category: 'Street',
+  engineCapacity: 125
+}
+
+export const noBuyValueMotorcyle = {
+  model: 'Honda CG Titan 125',
+  year: 1963,
+  color: 'red',
+  status: true,
+  category: 'Street',
+  engineCapacity: 125
+}
+
+export const noCategoryMotorcyle = {
+  model: 'Honda CG Titan 125',
+  year: 1963,
+  color: 'red',
+  status: true,
+  buyValue: 3500,
+  engineCapacity: 125
+}
+
+export const noEngineCapacityMotorcyle = {
+  model: 'Honda CG Titan 125',
+  year: 1963,
+  color: 'red',
+  status: true,
+  buyValue: 3500,
+  category: 'Street',
+}
+
+export const MotorcyleEngineLteTwo = {
+  model: 'Honda CG Titan 125',
+  year: 1963,
+  color: 'red',
+  status: true,
+  buyValue: 3500,
+  category: 'Street',
+  engineCapacity: 0
+}
+
+export const MotorcyleEngineGt2500 = {
+  model: 'Honda CG Titan 125',
+  year: 1963,
+  color: 'red',
+  status: true,
+  buyValue: 3500,
+  category: 'Street',
+  engineCapacity: 2501
+}
+
+export const MotorcyleCategoryNotString = {
+  model: 'Honda CG Titan 125',
+  year: 1963,
+  color: 'red',
+  status: true,
+  buyValue: 3500,
+  category: 1,
+  engineCapacity: 125
+}
+
+export const MotorcyleWrongCategory = {
+  model: 'Honda CG Titan 125',
+  year: 1963,
+  color: 'red',
+  status: true,
+  buyValue: 3500,
+  category: 'Passeio',
+  engineCapacity: 125
+}
