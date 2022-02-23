ef3888808d5044bc2524be7ae966222351ab80c1
Tiago Bovolin <tiagaoalb@gmail.com>
2022-02-23 15:55:11 -0300

    adiciona teste de motorcycle

--- tests/testAPI.test.ts
+++ tests/testAPI.test.ts
@@ -3,6 +3,7 @@ import request from 'supertest';
 import clearDatabase from './utils/clearDB';
 
 import * as carMock from './utils/CarsMock';
+import * as motorcycleMock from './utils/MotorcyclesMock';
 
 import server from '../src/server';
 
@@ -84,6 +85,84 @@ describe('1 - Crie a rota /cars', () => {
       const res = await request(server.getApp())
         .post('/cars')
         .send(carMock.validCar);
+      expect(res.statusCode).toEqual(201);
+    });
+  })
+});
+
+describe('2 - Crie a rota /motorcycles', () => {
+  describe('É possível criar um moto através de uma requisição POST para a rota /motorcycles', () => {
+    beforeAll(async () => {
+      await mongoose.connect(MONGO_URI);
+    });
+  
+    beforeEach(async () => {
+      await clearDatabase();
+    });
+  
+    afterAll(async () => {
+      await mongoose.disconnect();
+    });
+  
+    it('Não é possível criar um veículo Motorcycle com um objeto vazio', async () => {
+      const res = await request(server.getApp())
+        .post('/motorcycles')
+        .send({});
+      expect(res.statusCode).toEqual(400);
+    });
+
+    it('Não é possível criar uma moto com cilindrada inferior a 0', async () => {
+      const res = await request(server.getApp())
+        .post('/motorcycles')
+        .send(motorcycleMock.MotorcycleEngineLteTwo);
+      expect(res.statusCode).toEqual(400);
+    });
+
+    it('Não é possível criar uma moto com cilindrada superior a 2500', async () => {
+      const res = await request(server.getApp())
+        .post('/motorcycles')
+        .send(motorcycleMock.MotorcycleEngineGt2500);
+      expect(res.statusCode).toEqual(400);
+    });
+
+    it('Não é possível criar uma moto sem "model", "year", "color", "status" e "buyValue"', async () => {
+      let res = await request(server.getApp())
+        .post('/motorcycles')
+        .send(motorcycleMock.noModelMotorcycle);
+      expect(res.statusCode).toEqual(400);
+      res = await request(server.getApp())
+        .post('/motorcycles')
+        .send(motorcycleMock.noYearMotorcycle);
+      expect(res.statusCode).toEqual(400);
+      res = await request(server.getApp())
+        .post('/motorcycles')
+        .send(motorcycleMock.noColorMotorcycle);
+      expect(res.statusCode).toEqual(400);
+      res = await request(server.getApp())
+        .post('/motorcycles')
+        .send(motorcycleMock.noStatusMotorcycle);
+      expect(res.statusCode).toEqual(400);
+      res = await request(server.getApp())
+        .post('/motorcycles')
+        .send(motorcycleMock.noBuyValueMotorcycle);
+      expect(res.statusCode).toEqual(400);      
+    });
+
+    it('Não é possível criar uma moto sem "category" e "engineCapacity"', async () => {
+      let res = await request(server.getApp())
+        .post('/motorcycles')
+        .send(motorcycleMock.noCategoryMotorcycle);
+      expect(res.statusCode).toEqual(400);
+      res = await request(server.getApp())
+        .post('/motorcycles')
+        .send(motorcycleMock.noEngineCapacityMotorcycle);
+      expect(res.statusCode).toEqual(400);
+    });
+
+    it('É possível criar uma moto se todos os parametros forem passados corretamente', async () => {
+      const res = await request(server.getApp())
+        .post('/motorcycles')
+        .send(motorcycleMock.validMotorcycle);
       expect(res.statusCode).toEqual(201);
     });
   })
