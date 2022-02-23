c360fbbfa343095d439c3bc23ee3dfe4d7dcb6e9
Tiago Bovolin <tiagaoalb@gmail.com>
2022-02-23 14:57:38 -0300

    adiciona TrucksMock

---
+++ tests/utils/TrucksMock.ts
@@ -0,0 +1,102 @@
+export const validTruck = {
+  model: 'Uno da Escada',
+  year: 1963,
+  color: 'red',
+  status: true,
+  buyValue: 3500,
+  axlesQty: 2,
+  category: 'Urbano'
+};
+
+export const noModelTruck = {
+  year: 1963,
+  color: 'red',
+  status: true,
+  buyValue: 3500,
+  axlesQty: 2,
+  category: 'Urbano'
+};
+
+export const noYearTruck = {
+  model: 'Uno da Escada',
+  color: 'red',
+  status: true,
+  buyValue: 3500,
+  axlesQty: 2,
+  category: 'Urbano'
+}
+
+export const noColorTruck = {
+  model: 'Uno da Escada',
+  year: 1963,
+  status: true,
+  buyValue: 3500,
+  axlesQty: 2,
+  category: 'Urbano'
+}
+
+export const noStatusTruck = {
+  model: 'Uno da Escada',
+  year: 1963,
+  color: 'red',
+  buyValue: 3500,
+  axlesQty: 2,
+  category: 'Urbano'
+}
+
+export const noBuyValueTruck = {
+  model: 'Uno da Escada',
+  year: 1963,
+  color: 'red',
+  status: true,
+  axlesQty: 2,
+  category: 'Urbano'
+}
+
+export const noCategoryTruck = {
+  model: 'Uno da Escada',
+  year: 1963,
+  color: 'red',
+  status: true,
+  buyValue: 3500,
+  axlesQty: 2,
+}
+
+export const noAxlesTruck = {
+  model: 'Uno da Escada',
+  year: 1963,
+  color: 'red',
+  status: true,
+  buyValue: 3500,
+  category: 'Urbano'
+}
+
+export const TruckAxlesLtTwo = {
+  model: 'Uno da Escada',
+  year: 1963,
+  color: 'red',
+  status: true,
+  buyValue: 3500,
+  axlesQty: 1,
+  category: 'Urbano'
+}
+
+export const TruckAxlesGt5 = {
+  model: 'Uno da Escada',
+  year: 1963,
+  color: 'red',
+  status: true,
+  buyValue: 3500,
+  axlesQty: 6,
+  category: 'Urbano'
+}
+
+export const TruckWrongCategory = {
+  model: 'Uno da Escada',
+  year: 1963,
+  color: 'red',
+  status: true,
+  buyValue: 3500,
+  axlesQty: 5,
+  category: 'Guincho'
+}
