e2d904187a50595053e27eeb490a1ba43fedbcff
Tiago Bovolin <tiagaoalb@gmail.com>
2022-02-24 11:21:37 -0300

    exporta o car model

--- src/models/CarModel.ts
+++ src/models/CarModel.ts
@@ -1,4 +1,4 @@
-import { Schema, model as createModel, Document } from 'mongoose';
+import mongoose, { Schema, model as createModel, Document } from 'mongoose';
 
 import { Car } from '../interfaces/CarInterface';
 import { vehicleSchema } from './VehicleModel';
@@ -10,12 +10,15 @@ interface CarDocument extends Car, Document {}
 const carSchema = new Schema<CarDocument>({
   ...vehicleSchema.obj,
   seatsQty: Number,
-  doorsQty: Number,  
+  doorsQty: Number,
 }, { versionKey: false });
+
 class CarModel extends MongoModel<Car> {
   constructor(model = createModel('car', carSchema)) {
     super(model);
   }
 }
+
+export const Cars = mongoose.model('cars', carSchema);
 
 export default CarModel;
