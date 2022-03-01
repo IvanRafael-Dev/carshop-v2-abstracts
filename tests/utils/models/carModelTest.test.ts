fb9e57fbcaffbdd7e0c29e94ea6dba2dc4d7e695
Tiago <tiagaoalb@gmail.com>
2022-03-01 17:09:44 -0300

    fix carModelTest

--- tests/utils/models/carModelTest.test.ts
+++ tests/utils/models/carModelTest.test.ts
@@ -3,35 +3,41 @@ import mongoose from 'mongoose';
 
 import { Cars } from '../../../src/models/CarModel';
 
-import { validCar } from '../../sources/CarsMock';
+import { validCar, coverageCar } from '../../sources/CarsMock';
 
-const beforeAndAfter = (payload: object) => {
-  beforeAll(async () => {
-    (await mongoose.connect('mongodb://localhost:27017/CarShop')).Promise(payload);
-  });
+import { clearDatabase, closeDatabase } from '../../sources/db';
 
-  afterEach(async () => {
-    mongoose.connection.close();
-    mongoose.disconnect();
-  });
-};
+const databaseName = 'CarShop';
+
+const MONGO_URI = process.env.MONGO_URI
+  || `mongodb://localhost:27017/${databaseName}`;
 
 describe('Realiza testes na model de Carros', () => {
 
   // -----------------------|| CADASTRO DE CARROS ||-----------------------
   describe('Insere novo produto', () => {
-    const payloadProduct = validCar;
+    const payloadCar = validCar;
 
-    beforeAndAfter(payloadProduct);
+    beforeAll(async () => {
+      await mongoose.connect(MONGO_URI);
+    });
+  
+    beforeEach(async () => {
+      await clearDatabase();
+    });
+  
+    afterAll(async () => {
+      await closeDatabase();
+    });
 
     describe('Quando é inserido com sucesso', () => {
       it('retorna um objeto', async () => {
-        const response = await (payloadProduct);
+        const response = await Cars.create(payloadCar);
         expect(response).to.be.an('object');
       });
 
-      it('o objeto possui chaves: "id", "name" e "quantity"', async () => {
-        const response = await Cars.create(payloadProduct);
+      it('o objeto possui chaves: "model", "year" e "color", "status", "buyValue", "seatsQty", "doorsQty"', async () => {
+        const response = await Cars.create(payloadCar);
         expect(response).to.have.a.property('model');
         expect(response).to.have.a.property('year');
         expect(response).to.have.a.property('status');
@@ -44,18 +50,32 @@ describe('Realiza testes na model de Carros', () => {
 
   // -----------------------|| LISTA CARROS ||-----------------------
   describe('Busca carros', () => {
-    const payloadProduct = [validCar, validCar];
+    const payloadCar = coverageCar;
 
     describe('Buscando todos os carros', () => {
-      beforeAndAfter(payloadProduct);
+      beforeAll(async () => {
+        await mongoose.connect(MONGO_URI);
+      });
+    
+      beforeEach(async () => {
+        await clearDatabase();
+      });
+    
+      afterAll(async () => {
+        await closeDatabase();
+      });
 
       it('retorna um array com tamanho 2', async () => {
+        await Cars.create(payloadCar);
+        await Cars.create(payloadCar);
         const response = await Cars.find();
         expect(response).to.be.an('array').to.not.be.empty;
         expect(response).to.have.lengthOf(2);
       });
 
       it('os objetos do array contém as chaves: "model", "year" e "color", "status", "buyValue", "seatsQty", "doorsQty"', async () => {
+        await Cars.create(payloadCar);
+        await Cars.create(payloadCar);
         const response = await Cars.find();
         expect(response[0]).to.have.property('model');
         expect(response[0]).to.have.property('year');
@@ -76,15 +96,27 @@ describe('Realiza testes na model de Carros', () => {
   });
 
     describe('Buscando carro por "id"', () => {
-      const payloadProduct = [validCar, validCar];
-      beforeAndAfter(payloadProduct);
+      const payloadCar = validCar;
+      beforeAll(async () => {
+        await mongoose.connect(MONGO_URI);
+      });
+    
+      beforeEach(async () => {
+        await clearDatabase();
+      });
+    
+      afterAll(async () => {
+        await closeDatabase();
+      });
 
       it('retorna um objeto', async () => {
+        await Cars.create(payloadCar);
         const response = await Cars.findById(validCar._id);
         expect(response).to.be.an('object')
       });
 
       it('o objeto contém as chaves: "model", "year" e "color", "status", "buyValue", "seatsQty", "doorsQty"', async () => {
+        await Cars.create(payloadCar);
         const response = await Cars.findById(validCar._id);
         expect(response).to.have.property('model');
         expect(response).to.have.property('year');
@@ -98,17 +130,29 @@ describe('Realiza testes na model de Carros', () => {
 
   // -----------------------|| ALTERANDO CARROS ||-----------------------
   describe('Alterando carro', () => {
-    const payloadProduct = validCar;
+    const payloadCar = validCar;
 
-    beforeAndAfter(payloadProduct);
+    beforeAll(async () => {
+      await mongoose.connect(MONGO_URI);
+    });
+  
+    beforeEach(async () => {
+      await clearDatabase();
+    });
+  
+    afterAll(async () => {
+      await closeDatabase();
+    });
 
     describe('Quando é alterado com sucesso', () => {
       it('retorna um objeto', async () => {
+        await Cars.create(payloadCar);
         const response = await Cars.findOneAndUpdate(validCar._id);
         expect(response).to.be.an('object');
       });
 
       it('o objeto possui chaves: "model", "year" e "color", "status", "buyValue", "seatsQty", "doorsQty"', async () => {
+        await Cars.create(payloadCar);
         const response = await Cars.findOneAndUpdate(validCar._id);
         expect(response).to.have.property('model');
         expect(response).to.have.property('year');
@@ -123,25 +167,26 @@ describe('Realiza testes na model de Carros', () => {
 
   // -----------------------|| DELETANDO CARROS ||-----------------------
   describe('Deletando produto', () => {
-    const payloadProduct = validCar;
+    const payloadCar = validCar;
 
-    beforeAndAfter(payloadProduct);
+    beforeAll(async () => {
+      await mongoose.connect(MONGO_URI);
+    });
+  
+    beforeEach(async () => {
+      await clearDatabase();
+    });
+  
+    afterAll(async () => {
+      await closeDatabase();
+    });
 
     describe('Quando é deletado com sucesso', () => {
-      it('retorna um objeto', async () => {
-        const response = await Cars.deleteOne(validCar._id);
+      it('retorna um array vazio', async () => {
+        await Cars.create(payloadCar);
+        await Cars.deleteOne(validCar._id);
+        const response = await Cars.find();
         expect(response).to.be.an('array');
-      });
-
-      it('o objeto possui chaves: "model", "year" e "color", "status", "buyValue", "seatsQty", "doorsQty"', async () => {
-        const response = await Cars.deleteOne(validCar._id);
-        expect(response).to.have.property('model');
-        expect(response).to.have.property('year');
-        expect(response).to.have.property('color');
-        expect(response).to.have.property('status');
-        expect(response).to.have.property('buyValue');
-        expect(response).to.have.property('seatsQty');
-        expect(response).to.have.property('doorsQty');
       });
     });
   });
