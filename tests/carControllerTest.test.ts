3d5c6f9071c312ed336904a2473d86601bd2a8d6
Andre Vicente <isquicha@gmail.com>
2022-03-09 10:10:11 -0300

    Update controller tests

--- tests/carControllerTest.test.ts
+++ tests/carControllerTest.test.ts
@@ -1,141 +1,138 @@
-import { expect } from 'chai';
-import mongoose from 'mongoose';
-
-import { clearDatabase, closeDatabase } from '../src/__tests__/utils/db';
-import { validCar, coverageCar } from '../src/__tests__/utils/CarsMock';
-import { CarRequest, CarResponse } from '../src/protocols/http-protocol';
-
+import { coverageCar } from '../src/__tests__/utils/CarsMock';
 import CarControllers from '../src/controllers/CarController';
-import { Car } from '../src/interfaces/CarInterface'
+import connectToDatabase from '../src/models/connection';
+import { clearDatabase, closeDatabase } from '../src/__tests__/utils/db';
 
 const carController = new CarControllers();
 
-const databaseName = 'CarShop';
+beforeAll(async () => {
+  await connectToDatabase('mongodb://localhost:27017/CarShopTests');
+  await clearDatabase();
+});
 
-const MONGO_URI = process.env.MONGO_URI
-  || `mongodb://localhost:27017/${databaseName}`;
+afterAll(async () => {
+  await closeDatabase();
+});
 
 describe('Realizando testes na controller de carController', () => {
-  // -----------------------|| CADASTRANDO CARROS ||-----------------------
-    describe('Insere novo carro', () => {
-      describe('quando é inserido com sucesso', () => {
-        const payloadCar = coverageCar;
-        const req: CarRequest<Car> = { body: payloadCar };
-        const res: CarResponse<Car> = { statusCode: 201 }
-        beforeEach(async () => {
-          await mongoose.connect(MONGO_URI);
-          await clearDatabase();
-        });
-      
-        afterEach(async () => {
-          await closeDatabase();
-        });
+  describe('Insere novo carro', () => {
+    describe('quando é inserido com sucesso', () => {
+      const mockResponse: any = {};
+      mockResponse.json = jest.fn();
+      mockResponse.status = jest.fn().mockReturnValue(mockResponse);
+
+      const mockRequest: any = { body: coverageCar };
 
       it('é retornado o status 201', async () => {
-        await carController.create(req, res);
+        await carController.create(mockRequest, mockResponse);
 
-        expect(res.statusCode).to.be.equal(true);
+        expect(mockResponse.status).toHaveBeenCalledTimes(1);
+        expect(mockResponse.status).toHaveBeenCalledWith(201);
+        expect(mockResponse.json).toHaveBeenCalledTimes(1);
+        // expect(mockResponse.json).toHaveBeenCalledWith({
+        //   ...coverageCar,
+        //   _id: expect.any(String),
+        // });
       });
-
     });
   });
 
-//   // -----------------------|| LISTANDO CARROS ||-----------------------
-//   describe('Buscando carros', () => {
-//     describe('quando é buscado todos carros', () => {
-//       const res = {};
-//       const req = {};
+  //   // -----------------------|| LISTANDO CARROS ||-----------------------
+  //   describe('Buscando carros', () => {
+  //     describe('quando é buscado todos carros', () => {
+  //       const res = {};
+  //       const req = {};
 
-//       before(() => {
-//         res.status = sinon.stub().returns(res);
-//         res.json = sinon.stub().returns([{ id: 1, ...payloadProduct }]);
-//         sinon.stub(productSrv, 'getAll').resolves([{ id: 1, ...payloadProduct }]);
-//       });
+  //       before(() => {
+  //         res.status = sinon.stub().returns(res);
+  //         res.json = sinon.stub().returns([{ id: 1, ...payloadProduct }]);
+  //         sinon.stub(productSrv, 'getAll').resolves([{ id: 1, ...payloadProduct }]);
+  //       });
 
-//       after(() => {
-//         productSrv.getAll.restore();
-//       });
+  //       after(() => {
+  //         productSrv.getAll.restore();
+  //       });
 
-//       it('é retornado o status 200', async () => {
-//         await carController.getAll(req, res);
+  //       it('é retornado o status 200', async () => {
+  //         await carController.getAll(req, res);
 
-//         expect(res.status.calledWith(200)).to.be.equal(true);
-//       });
-//     });
+  //         expect(res.status.calledWith(200)).to.be.equal(true);
+  //       });
+  //     });
 
-//     describe('quando é buscado carro por id', () => {
-//       const res = {};
-//       const req = {};
+  //     describe('quando é buscado carro por id', () => {
+  //       const res = {};
+  //       const req = {};
 
-//       before(() => {
-//         req.params = { id: VALID_ID };
-//         res.status = sinon.stub().returns(res);
-//         res.json = sinon.stub().returns({ id: 1, ...payloadProduct });
-//         sinon.stub(productSrv, 'getById').resolves({ id: 1, ...payloadProduct });
-//       });
+  //       before(() => {
+  //         req.params = { id: VALID_ID };
+  //         res.status = sinon.stub().returns(res);
+  //         res.json = sinon.stub().returns({ id: 1, ...payloadProduct });
+  //         sinon.stub(productSrv, 'getById').resolves({ id: 1, ...payloadProduct });
+  //       });
 
-//       after(() => {
-//         productSrv.getById.restore();
-//       });
+  //       after(() => {
+  //         productSrv.getById.restore();
+  //       });
 
-//       it('é retornado o status 200', async () => {
-//         await carController.getById(req, res);
+  //       it('é retornado o status 200', async () => {
+  //         await carController.getById(req, res);
 
-//         expect(res.status.calledWith(200)).to.be.equal(true);
-//       });
-//     });
-//   });
+  //         expect(res.status.calledWith(200)).to.be.equal(true);
+  //       });
+  //     });
+  //   });
 
-//   // -----------------------|| ALTERANDO PRODUTO ||-----------------------
-//   describe('Alterando novo carro', () => {
-//     describe('quando é Alterado com sucesso', () => {
-//       const res = {};
-//       const req = {};
+  //   // -----------------------|| ALTERANDO PRODUTO ||-----------------------
+  //   describe('Alterando novo carro', () => {
+  //     describe('quando é Alterado com sucesso', () => {
+  //       const res = {};
+  //       const req = {};
 
-//       before(() => {
-//         req.params = { id: VALID_ID };
-//         req.body = payloadProduct;
-//         res.status = sinon.stub().returns(res);
-//         res.json = sinon.stub().returns({ id: 1, ...payloadProduct });
-//         sinon.stub(productSrv, 'update').resolves({ id: 1, ...payloadProduct });
-//       });
+  //       before(() => {
+  //         req.params = { id: VALID_ID };
+  //         req.body = payloadProduct;
+  //         res.status = sinon.stub().returns(res);
+  //         res.json = sinon.stub().returns({ id: 1, ...payloadProduct });
+  //         sinon.stub(productSrv, 'update').resolves({ id: 1, ...payloadProduct });
+  //       });
 
-//       after(() => {
-//         productSrv.update.restore();
-//       });
+  //       after(() => {
+  //         productSrv.update.restore();
+  //       });
 
-//       it('é retornado o status 200', async () => {
-//         await carController.update(req, res);
+  //       it('é retornado o status 200', async () => {
+  //         await carController.update(req, res);
 
-//         expect(res.status.calledWith(200)).to.be.equal(true);
-//       });
+  //         expect(res.status.calledWith(200)).to.be.equal(true);
+  //       });
 
-//     });
-//   });
+  //     });
+  //   });
 
-//    // -----------------------|| DELETANDO CARRO ||-----------------------
-//    describe('Deletando carros', () => {
-//     describe('quando é deletado um carro', () => {
-//       const res = {};
-//       const req = {};
+  //    // -----------------------|| DELETANDO CARRO ||-----------------------
+  //    describe('Deletando carros', () => {
+  //     describe('quando é deletado um carro', () => {
+  //       const res = {};
+  //       const req = {};
 
-//       before(() => {
-//         req.params = { id: VALID_ID };
-//         res.status = sinon.stub().returns(res);
-//         res.json = sinon.stub().returns({ id: 1, ...payloadProduct });
-//         sinon.stub(productSrv, 'drop').resolves([{ id: 1, ...payloadProduct }]);
-//       });
+  //       before(() => {
+  //         req.params = { id: VALID_ID };
+  //         res.status = sinon.stub().returns(res);
+  //         res.json = sinon.stub().returns({ id: 1, ...payloadProduct });
+  //         sinon.stub(productSrv, 'drop').resolves([{ id: 1, ...payloadProduct }]);
+  //       });
 
-//       after(() => {
-//         productSrv.drop.restore();
-//       });
+  //       after(() => {
+  //         productSrv.drop.restore();
+  //       });
 
-//       it('é retornado o status 204', async () => {
-//         await carController.drop(req, res);
+  //       it('é retornado o status 204', async () => {
+  //         await carController.drop(req, res);
 
-//         expect(res.status.calledWith(204)).to.be.equal(true);
-//       });
-//     });
-//   });
-// });
+  //         expect(res.status.calledWith(204)).to.be.equal(true);
+  //       });
+  //     });
+  //   });
+  // });
 });
