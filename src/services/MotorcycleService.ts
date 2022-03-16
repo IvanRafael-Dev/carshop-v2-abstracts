7e7d52e5e7a1760c9365f3ba4369cb39a08626a4
Tiago Bovolin <tiagaoalb@gmail.com>
2022-03-16 11:52:18 -0300

    add category validation using Zod

--- src/services/MotorcycleService.ts
+++ src/services/MotorcycleService.ts
@@ -2,22 +2,12 @@ import Service from './index';
 import { Motorcycle } from '../interfaces/MotorcycleInterface';
 import MotorcycleModel from '../models/MotorcycleModel';
 
-const categoryError = 'Category must be "Street", "Custom" or "Trail"';
-
 class MotorcycleService extends Service<Motorcycle> {
   constructor(model = new MotorcycleModel()) {
     super(model);
   }
 
   public async create(obj: Motorcycle): Promise<Motorcycle | null> {
-    const { category } = obj;
-    if (
-      category !== 'Street'
-      && category !== 'Custom'
-      && category !== 'Trail'
-    ) {
-      throw new Error(categoryError);
-    }
     if (obj) {
       return this.model.create(obj);
     }
