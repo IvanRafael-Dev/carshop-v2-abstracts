14e814fe88c761cd457f68da513b9ec8415466f2
Tiago <tiagaoalb@gmail.com>
2022-03-16 21:52:16 -0300

    fix imports and change requirement number

--- __tests__/motorcycleTests/15 - deleteMotorcycle.test.ts
+++ __tests__/motorcycleTests/15 - deleteMotorcycle.test.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import request from 'supertest';
 import { clearDatabase, closeDatabase } from '../utils/db';
 
-import * as carMock from '../utils/CarsMock';
+import * as motorcycleMock from '../utils/MotorcyclesMock';
 
 import server from '../../src/server';
 
@@ -12,7 +12,7 @@ const MONGO_URI = process.env.MONGO_URI
   || `mongodb://localhost:27017/${databaseName}`;
 
 
-describe('10 - Utilize uma requisição DELETE para o endpoint /cars/id para excluir os registros de um carro', () => {
+describe('15 - Utilize uma requisição DELETE para o endpoint /motorcycles/id para excluir os registros de uma moto', () => {
   beforeAll(async () => {
     await mongoose.connect(MONGO_URI);
   });
@@ -28,8 +28,8 @@ describe('10 - Utilize uma requisição DELETE para o endpoint /cars/id para exc
   it('Será verificado que é disparado o erro 404 "Object not found" caso o id possua 24 caracteres mas é inválido ', async () => {
     const errorMsg = { error: "Object not found" };
     const response = await request(server.getApp())
-      .put('/cars/999999999999999999999999')
-      .send(carMock.validCar);
+      .put('/motorcycles/999999999999999999999999')
+      .send(motorcycleMock.validMotorcycle);
 
     expect(response.status).toBe(404);
     expect(response.body.error).toBe(errorMsg.error);
@@ -38,21 +38,21 @@ describe('10 - Utilize uma requisição DELETE para o endpoint /cars/id para exc
   it('Será verificado que é disparado o erro 400 "Id must have 24 hexadecimal characters" caso o id possua menos que 24 caracteres', async () => {
     const errorMsg = { error: "Id must have 24 hexadecimal characters" }
     const response = await request(server.getApp())
-      .put('/cars/99999')
-      .send(carMock.validCar);
+      .put('/motorcycles/99999')
+      .send(motorcycleMock.validMotorcycle);
     expect(response.status).toBe(400);
     expect(response.body.error).toBe(errorMsg.error);
   });
 
-  it('Será verificado que um carro é removido com sucesso', async () => {
+  it('Será verificado que uma moto é removida com sucesso', async () => {
     const res = await request(server.getApp())
-      .post('/cars')
-      .send(carMock.validCar)
+      .post('/motorcycles')
+      .send(motorcycleMock.validMotorcycle)
 
     const { _id } = res.body;
 
     const result = await request(server.getApp())
-      .del(`/cars/${_id}`);
+      .del(`/motorcycles/${_id}`);
 
     expect(result.statusCode).toEqual(204);
     expect(result.body).toEqual({});
