9ada3924f34b6442997b78c8aa5bbd4942f27085
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2023-04-24 12:47:22 -0300

    feat: add Routers to ap

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import ErrorHandler from './Middlewares/ErrorHandler';
+import carRoutes from './Routes/carRouter';
+import motorcycleRoutes from './Routes/motorcycleRouter';
 
 const app = express();
 app.use(express.json());
+app.use(carRoutes);
+app.use(motorcycleRoutes);
 app.use(ErrorHandler.execute);
 
 export default app;
